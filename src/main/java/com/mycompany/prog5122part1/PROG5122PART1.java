5c1d5b0ca1fc0717c9eb4631473ed2735606a0cd
RC_Student_lab <RC_Student_lab@10.0.0.217>
2024-09-09 07:59:37 +0000

    First Commit

---
+++ src/main/java/com/mycompany/prog5122part1/PROG5122PART1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5122part1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class PROG5122PART1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+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
